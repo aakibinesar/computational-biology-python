107a34c60c3d7776dcf1f177c207fb8f79b5b32b
Aakib Bin Nesar <aakibinesar@users.noreply.github.com>
2026-07-09 15:38:58 +0600

    Add files via upload

---
+++ original_rosalind_tracks/bioinformatics_textbook_track/chapter_06/rosalind_ba6b.py
@@ -0,0 +1,27 @@
+def read_data(file_name):
+    with open(file_name, 'r') as file:
+        line = file.readline().strip().split()
+
+    length = len(line)
+    line[0] = line[0].replace("(", "")
+    line[length-1] = line[length-1].replace(")", "")
+    for i in xrange(len(line)):
+        line[i] = int(line[i])
+
+    line = [0] + line + [length+1]
+    return line
+
+def br_points_num(data):
+    num = 0
+    length = len(data)
+    for i in xrange(length-1):
+        if data[i] == data[i+1] - 1:
+            continue
+        else:
+            num += 1
+
+    print num
+
+if __name__ == "__main__":
+    data = read_data("rosalind_ba6b.txt")
+    br_points_num(data)

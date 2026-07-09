1f45813e44aaceda79aa4a52e08fe7f211ab368f
Aakib Bin Nesar <aakibinesar@users.noreply.github.com>
2026-07-09 15:39:29 +0600

    Add files via upload

---
+++ original_rosalind_tracks/bioinformatics_textbook_track/chapter_07/rosalind_ba7b.py
@@ -0,0 +1,27 @@
+import sys
+
+def read_data(file_name):
+    with open(file_name, 'r') as file:
+        n = int(file.readline().strip())
+        j = int(file.readline().strip())
+        D = []
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip().split()
+            D.append([int(elem) for elem in line])
+
+        return D, j
+
+def limb_length(D, j):
+    limb_len = sys.maxint
+    for i in xrange(len(D)):
+        for k in xrange(len(D[i])):
+            if (i != j) and (j != k) and (i != k):
+                temp = (D[i][j] + D[j][k] - D[i][k])/2
+                limb_len = min(limb_len, temp)
+
+    return limb_len
+
+if __name__ == "__main__":
+    D, j = read_data("rosalind_ba7b.txt")
+    print limb_length(D, j)
